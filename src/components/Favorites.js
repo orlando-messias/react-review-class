7ce62c4499403e5fef03eeb279bffb5039da1a85
Orlando <programadorlando@gmail.com>
2020-06-29 23:51:59 -0300

    Create Favorites.js

---
+++ src/components/Favorites.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Favorite Pokémons</h2>
+      </div>
+    );
+  }
+}
+
+export default Favorites;
